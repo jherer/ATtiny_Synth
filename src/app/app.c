d77fb61614da2bca9c9b20be02632c5ded18ccbc
jherer <joshuadherer@icloud.com>
2026-01-25 22:32:39 -0500

    Complete merge on app.c

--- src/app/app.c
+++ src/app/app.c
@@ -17,16 +17,6 @@
 
 /* Add state variables in this struct */
 typedef struct {
-<<<<<<< HEAD
-
-} app_state_t;
-
-static app_state_t state = {0}; // Always zero initialize state by default
-
-error_t app_init(void) {
-    /* Configure drivers, services, and initial state here */
-    
-=======
     uint8_t a;
 } app_state_t;
 
@@ -34,20 +24,13 @@ static app_state_t state = {0};
 
 error_code_t app_init(void) {
     /* Configure drivers and services here */
->>>>>>> a1f2091 (Added a simulator for testing errors and register config. Cannot do interrupts or simulate gpio inputs as of yet. Also, revert some untested modules.)
 
     interrupts_enable();
     return ERROR_OK;
 }
 
-<<<<<<< HEAD
-error_t app_run(void) {
-    /* Run the main loop of the program here */
-
-=======
 error_code_t app_run(void) {
     /* Run the main loop of the program here */
->>>>>>> a1f2091 (Added a simulator for testing errors and register config. Cannot do interrupts or simulate gpio inputs as of yet. Also, revert some untested modules.)
     
     return ERROR_OK;
 }
